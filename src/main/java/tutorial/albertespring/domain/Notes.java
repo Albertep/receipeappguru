f3ac28ca90a568044b17025f901c76ebe9e064d3
Albertep <alberte9494@gmail.com>
2020-10-24 19:33:59 +0200

    Initial commit

---
+++ src/main/java/tutorial/albertespring/domain/Notes.java
@@ -0,0 +1,41 @@
+package tutorial.albertespring.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+
+public class Notes {
+
+    private Long id;
+
+
+    private Recipe recipe;
+
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
